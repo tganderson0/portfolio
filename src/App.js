951fb7e3dcd0837f10e5033206fa87946afe7e92
Taylor Anderson <tganderson0@gmail.com>
2024-01-22 20:41:54 -0700

    got terminal working

--- src/App.js
+++ src/App.js
@@ -11,11 +11,28 @@ function App() {
 
   return (
     <BrowserRouter>
-      <NavigationBar setMenuOpen={setMenuOpen}/>
-      <Routes>
-        <Route exact path='/' element={<Home/>} />
-        <Route path='*' element={<ErrorPage/>} />
-      </Routes>
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+        <label htmlFor="my-drawer" className="btn btn-ghost drawer-button text-3xl m-2"><i className="fa-solid fa-bars"></i></label>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='*' element={<ErrorPage />} />
+        </Routes>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+            <li><a>Home</a></li>
+            <li><a>About Me</a></li>
+            <li><a>Projects</a></li>
+            <li><a><i className="fa-brands fa-github text-4xl"></i></a></li>
+            <li><a><i className="fa-brands fa-linkedin text-4xl"></i></a></li>
+          </ul>
+        </div>
+      </div>
+
+      
     </BrowserRouter>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,61 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const finishedLines = [
+  "./about_self.sh",
+  "starting...",
+  "Hello, I'm Taylor",
+  "I'm a current Master's student, focused on Robotics and Computer Graphics.",
+  "I have a background in various programming languages, like C++, Python, C# and JavaScript."
+];
 
 const Home = () => {
+  const [line0, setLine0] = useState("");
+  const [line1, setLine1] = useState("");
+  const [line2, setLine2] = useState("");
+  const [line3, setLine3] = useState("");
+  const [line4, setLine4] = useState("");
+
+  const [currentLine, setCurrentLine] = useState(0);
+
+
+
+  const handleType = () => {
+    const setLineFunctions = [setLine0, setLine1, setLine2, setLine3, setLine4];
+    const allLines = [line0, line1, line2, line3, line4];
+
+    if (allLines[currentLine].length !== finishedLines[currentLine].length){
+      setLineFunctions[currentLine](oldLine => oldLine.concat(finishedLines[currentLine].charAt(oldLine.length)));
+    }
+    else {
+      if (currentLine === 4)
+        return;
+      setCurrentLine(oldLine => oldLine + 1);
+    }
+  };
+
+  useEffect(() => {
+    if (line4.length === finishedLines[4].length) return;
+    if (currentLine < 5) {
+      setTimeout(handleType, 50);
+    }
+  }, [line0, line1, line2, line3, line4, currentLine]);
+
+  useEffect(() => {
+    handleType();
+  }, []);
+
   return(
-    <div>
-      HOME
+    <div className="p-2">
+      <div>
+        <div className="mockup-code pl-2">
+          <pre data-prefix="$"><code>{line0}</code></pre>
+          {line1.length > 0 && <pre data-prefix=">" className="text-warning whitespace-pre-wrap"><code>{line1}</code></pre>}
+          {line2.length > 0 && <pre data-prefix=">" className="text-warning whitespace-pre-wrap"><code>{line2}</code></pre>}
+          {line3.length > 0 && <pre data-prefix=">" className="text-success whitespace-pre-wrap"><code>{line3}</code></pre>}
+          {line4.length > 0 && <pre data-prefix=">" className="text-success whitespace-pre-wrap"><code>{line4}</code></pre>}
+        </div>
+      </div>
     </div>
   )
 }
